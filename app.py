baae3c9bf892881daad4d532e734324600032b3c
asbruap2426j0090 <24bscit016.cas@ppg.edu.in>
2026-09-10 10:34:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(_name_)
+@app.route("/")
+def home():
+  return "hello from docker!my first container is running."
+if _name_ == "_main";
+app.run(host="0.0.0.0",port=5000)
